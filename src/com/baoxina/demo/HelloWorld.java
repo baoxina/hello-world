bff05f2189c336c4e45dd712227b4c98c32ff787
包鑫 <bx920704@qq.com>
2017-05-15 15:23:48 +0800

    Initial commit

---
+++ src/com/baoxina/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.baoxina.demo;
+
+/**
+ * Created by baoxina on 2017/5/15.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello world");
+    }
+}
